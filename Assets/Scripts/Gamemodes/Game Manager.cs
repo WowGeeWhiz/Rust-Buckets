55a8e8382ad920e4d5c90d0b6d81e02804a86800
coolguy2402 <jake.hundley.2002@gmail.com>
2024-09-15 13:59:42 -0400

    CTF additions
    made it more of a complete version. Dropped flags return and there's more backend work.

--- Assets/Scripts/Gamemodes/Game Manager.cs
+++ Assets/Scripts/Gamemodes/Game Manager.cs
@@ -6,39 +6,43 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    /*
-     * Not sure what to do with this script right now
-     * A "game manager" will be needed to determine what game objects need to be enabled on the stage depending on the game mode
-     * It will need to be intertwined with the menu so I'm not sure how to implement it until we have a concrete plan for how the menus work
-     * I figured this would also keep track of scores and maybe player stats if we decide to track that
-     * All of this code will probably be deleted and reworked
-     */
     public int redTeamScore;
     public int blueTeamScore;
+
+    public int targetScore;
+
+    public string mode;
 
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
+        mode = "CTF";
+        //DontDestroyOnLoad(this.gameObject);
         redTeamScore = 0;
         blueTeamScore = 0;
+
+        if (mode.Equals("CTF"))
+        {
+            //enable all CTF game objects
+            GameObject.Find("CTF").SetActive(true);
+            targetScore = 3;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void SelectedGameMode(string mode)
-    {
-        if (mode.Equals("CTF"))
+        if(blueTeamScore >= targetScore)
         {
-            this.AddComponent<CaptureTheFlag>();
+            EndScreen("blue");
+        }
+        if(redTeamScore >= targetScore)
+        {
+            EndScreen("red");
         }
     }
 
-    public void EndScreen()
+    public void EndScreen(string team)
     {
 
     }
